68d97333b7d968b9cae1d5c03669e7ec8cd61d28
Dibyendu <dibyendu230@gmail.com>
2020-05-20 11:44:25 +0530

    Updates

---
+++ precache-manifest.c4b9f1ad92b85eae7aac27f104cb3be9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3469b8fd7579e3d7274584e31520dd3a",
+    "url": "/novel-covid19/index.html"
+  },
+  {
+    "revision": "b9401006dcc685a74c6d",
+    "url": "/novel-covid19/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "cc55be847303e326351a",
+    "url": "/novel-covid19/static/css/main.aa188322.chunk.css"
+  },
+  {
+    "revision": "b9401006dcc685a74c6d",
+    "url": "/novel-covid19/static/js/2.71c5b9b1.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/novel-covid19/static/js/2.71c5b9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc55be847303e326351a",
+    "url": "/novel-covid19/static/js/main.628e0b8e.chunk.js"
+  },
+  {
+    "revision": "5130f8d79821743cadfc",
+    "url": "/novel-covid19/static/js/runtime-main.c862e270.js"
+  }
+]);
